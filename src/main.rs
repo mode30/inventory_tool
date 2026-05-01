a7ddcce30dc03f77cbf6056be88ea6a1a895babe
mode30 <emmanuelmodebelu@gmail.com>
2026-05-01 12:55:38 +0100

    added add new product and quantity function

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,7 @@ struct Inventory {
     inventory: Vec<HashMap<String, i32>>,
 }
 
-fn main() {
-    let mut new_object: HashMap<String, i32> = HashMap::new();
-    new_object.insert(String::from("battery"), 12);
-
-    let _object_bucket = vec![new_object.clone()];
-
-    let product = user_query("enter product:").unwrap_or("null".to_string());
-    let quantity = atoi("enter quantity").unwrap_or_default();
-    let new_product = product;
-    let new_quantity = quantity;
-    new_object.insert(new_product, new_quantity);
-}
+fn main() {}
 
 #[allow(dead_code)]
 fn user_query(prompt: &str) -> Result<String, io::Error> {
@@ -37,4 +26,47 @@ fn atoi(prompt: &str) -> Result<i32, io::Error> {
         .parse()
         .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "nan"))?;
     Ok(buffer)
+}
+
+// impl Inventory {
+//     // fn new()
+//     //
+//     fn diplap_inventory(&self,)
+// }
+//
+// #[allow(unused_variableslint)]
+#[allow(dead_code)]
+fn add_new_product(prompt: &str) {
+    print!("prompt:{}", prompt);
+    std::io::stdout().flush().unwrap_or_default();
+    loop {
+        let mut new_object: HashMap<String, i32> = HashMap::new();
+        new_object.insert(String::from("battery"), 12);
+        new_object.insert("keyboard".to_string(), 4);
+
+        let product = user_query("enter product:").unwrap_or("null".to_string());
+        let quantity = atoi("enter quantity").unwrap_or_default();
+        let new_product = product;
+        let new_quantity = quantity;
+
+        new_object.insert(new_product.clone(), new_quantity);
+        let object_bucket: Vec<HashMap<String, i32>> = vec![new_object];
+        // let mut object_bucket: Vec<HashMap<String, i32>> = Vec::new();
+        //
+        // // object_bucket = vec![new_object.clone()];
+        // object_bucket.push(new_object.clone());
+        // new_object.insert(new_product, new_quantity);
+
+        // Inventory:object_bucket.push(new_object.insert(new_product, new_quantity));
+        // let object_file = Inventory {
+        //     inventory: vec![
+        //         // new_object.insert(new_product, new_quantity),
+        //         HashMap::from(new_product, new_quantity),
+        //     ], // Inventory:object_bucket.push(new_object.insert(new_product, new_quantity));
+        // };
+
+        for values in &object_bucket {
+            println!("values:{:?}", values);
+        }
+    }
 }
