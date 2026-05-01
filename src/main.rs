83f9bdafa6a5cdb1aae377409789b373ba12999c
mode30 <emmanuelmodebelu@gmail.com>
2026-05-01 12:03:35 +0100

    add user input and quantity

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 use std::{
     collections::HashMap,
-    io::{self, Read, Write},
+    io::{self, Write},
 };
 
 #[allow(dead_code)]
 struct Inventory {
-    inventory: Vec<HashMap<String, u16>>,
+    inventory: Vec<HashMap<String, i32>>,
 }
 
 fn main() {
-    let mut new_object: HashMap<String, u16> = HashMap::new();
+    let mut new_object: HashMap<String, i32> = HashMap::new();
     new_object.insert(String::from("battery"), 12);
 
-    let _object_bucket = vec![new_object];
-    // println!
+    let _object_bucket = vec![new_object.clone()];
+
+    let product = user_query("enter product:").unwrap_or("null".to_string());
+    let quantity = atoi("enter quantity").unwrap_or_default();
+    let new_product = product;
+    let new_quantity = quantity;
+    new_object.insert(new_product, new_quantity);
 }
 
 #[allow(dead_code)]
@@ -23,4 +28,13 @@ fn user_query(prompt: &str) -> Result<String, io::Error> {
     let mut user_input = String::new();
     std::io::stdin().read_line(&mut user_input)?;
     Ok(user_input.trim().to_string())
+}
+
+fn atoi(prompt: &str) -> Result<i32, io::Error> {
+    let buffer = user_query(prompt)?;
+    let buffer: i32 = buffer
+        .trim()
+        .parse()
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "nan"))?;
+    Ok(buffer)
 }
